5cfe1ad1b039d411e9f77104939a973014588c26
ABDUL LATHEEF <latheef.mitco@gmail.com>
2024-11-28 17:43:42 +0530

    image uplaoding and downloading

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const exp = require("express");
 const bodyParser = require('body-parser')
 const cors = require("cors");
 const multer = require("multer");
+const path = require('path');
 
 
 const app = exp();
@@ -11,15 +12,45 @@ app.use(cors());
 
 app.use(bodyParser.json())
 
+app.use(exp.static("images"));
+
 var list = [];
 
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'images/')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now(); // Add a unique timestamp
+        const extension = path.extname(file.originalname); // Extract file extension
+        cb(null, file.fieldname + '-' + uniqueSuffix + extension); 
+    }
+  })
+// Initialize Multer with Storage
+const upload = multer({storage:storage})
+
+// Route to Handle Single File Upload
+app.post('/upload', upload.single('image'), (req, res) => {
+    try {
+        // Access the uploaded file via req.file
+        console.log(req.file);
+        res.status(200).json({
+            message: 'Image uploaded successfully!',
+            image: req.file.filename
+        });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
 
 
-app.use(exp.static("images"));
 
 
-app.get("/",(req,res)=>{
+
+
+
+app.get("/", (req, res) => {
     res.send("Welcome to Oxdo Technologies")
 })
 
@@ -27,24 +58,24 @@ app.get("/",(req,res)=>{
 
 
 app.get("/posts", (req, res) => {
-    
+
     try {
-        if(req.headers["key"]!="oxdo") throw "No heder"
+        if (req.headers["key"] != "oxdo") throw "No heder"
 
         res.send(list);
-        
+
     } catch (error) {
         console.log(error);
-        res.status(400).send(error); 
+        res.status(400).send(error);
 
     }
-    
+
 })
 
 app.post("/post", (req, res) => {
 
     try {
-        if(req.headers["key"]!="oxdo") throw "No heder"
+        if (req.headers["key"] != "oxdo") throw "No heder"
         const post = req.body
 
         console.log(Object.keys(post).length);
@@ -59,7 +90,7 @@ app.post("/post", (req, res) => {
         res.send(post);
     } catch (e) {
         console.log(e);
-        res.status(400).send(error); 
+        res.status(400).send(error);
 
     }
 })
@@ -67,21 +98,21 @@ app.post("/post", (req, res) => {
 app.put("/post/:id", (req, res) => {
 
     try {
-        if(req.headers["key"]!="oxdo") throw "No heder"
+        if (req.headers["key"] != "oxdo") throw "No heder"
         const id = req.params.id;
-        console.log("params id:- "+id);
+        console.log("params id:- " + id);
 
         const post = req.body
 
-        
+
         if (Object.keys(post).length < 3) {
             throw "Error";
         }
         console.log("incoming post:- ");
         console.log(post);
-        
 
-       
+
+
 
 
 
@@ -98,59 +129,59 @@ app.put("/post/:id", (req, res) => {
         });
 
         console.log("list");
-        list.forEach(v=>{
+        list.forEach(v => {
             console.log(v.title);
-            
+
         })
-        
+
 
         res.send(list);
     } catch (e) {
         console.log(e);
-        res.status(400).send(error); 
+        res.status(400).send(error);
 
     }
 })
 
 app.delete("/post/:id", (req, res) => {
     try {
-        if(req.headers["key"]!="oxdo") throw "No heder"
+        if (req.headers["key"] != "oxdo") throw "No heder"
         const id = req.params.id;
         console.log(id);
 
         const newList = list.filter(obj => obj.id != id);
-        
+
         list = newList;
-        
+
         res.send(list);
 
     } catch (error) {
         console.log(error);
-        res.status(400).send(error); 
+        res.status(400).send(error);
 
     }
 })
 
 
-app.get("/search",(req,res)=>{
+app.get("/search", (req, res) => {
     try {
-        if(req.headers["key"]!="oxdo") throw "No heder"
-        const {searchText} = req.query || "";
+        if (req.headers["key"] != "oxdo") throw "No heder"
+        const { searchText } = req.query || "";
 
         console.log(searchText);
 
-        const newList = list.filter((obj)=>{
-            if(obj.title.includes(searchText)){
+        const newList = list.filter((obj) => {
+            if (obj.title.includes(searchText)) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
         })
-        
+
         res.send(newList);
     } catch (error) {
         console.log(error);
-        res.status(400).send(error); 
+        res.status(400).send(error);
     }
 })
 
